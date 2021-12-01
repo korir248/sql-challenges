ffcb51c6a2e36572dc66d33aae26ff0a2a9457c8
Eugene Korir <62384233+korir248@users.noreply.github.com>
2021-12-01 11:53:24 +0300

    update interviews

--- interviews(task 3).sql
+++ interviews(task 3).sql
@@ -1,4 +1,6 @@
-select con.contest_id, con.hacker_id, con.name, sum(s.total_submissions) total_submissions, sum(s.total_accepted_submissions) total_accepted_submissions, sum(v.total_views) total_views, sum(v.total_unique_views) total_unique_views
+select con.contest_id, con.hacker_id, con.name, sum(s.total_submissions) total_submissions, 
+sum(s.total_accepted_submissions) total_accepted_submissions, sum(v.total_views) total_views,
+sum(v.total_unique_views) total_unique_views
 from Contests con
 inner join Colleges c on con.contest_id = c.contest_id
 inner join Challenges ch on ch.college_id = c.college_id
